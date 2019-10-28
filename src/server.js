014f38958d102780b68561c2678299a7c401c6ce
Zohurul Islam <zohurul88@gmail.com>
2019-10-28 22:24:22 +0600

    just code refactor

--- src/server.js
+++ src/server.js
@@ -2,15 +2,16 @@ import express from "express";
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import { decodeJWT } from "./app/jwt";
-import errorRes from "./app/error"
+import errorRes from "./app/error";
+import config from "../config/config";
 
 dotenv.config();
 const app = express();
-const excludeAUthCheck = require("../config/config");
+
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-    if (excludeAUthCheck.indexOf(req.method + req.url) > -1) {
+    if (config.excludeAuthCheck.indexOf(req.method + req.url) > -1) {
         return next();
     }
     if (!req.headers.authorization) {
@@ -24,9 +25,16 @@ app.use((req, res, next) => {
     return next();
 });
 
+/**
+ * user module
+ */
 app.use("/user", require("./modules/user"));
 
 
+
+/**
+ * Fallback for 404
+ */
 app.all("*", (req, res) => {
     return res.status(404).json({
         errors: [{
