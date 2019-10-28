14789ed69a4231ef13c2739e0c0e70fff2235032
Zohurul Islam <zohurul88@gmail.com>
2019-10-28 17:05:49 +0600

    refresh token added

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { MongoClient } from "mongodb";
+import { ObjectID, MongoClient } from "mongodb";
 import bodyParser from 'body-parser';
 import bcrypt from 'bcrypt';
 
 const jwt = require('jsonwebtoken');
+const extend = require('extend');
 const dotenv = require('dotenv');
 dotenv.config();
 const Ajv = require('ajv');
@@ -16,7 +17,67 @@ const dbName = 'recharge';
 
 app.use(bodyParser.json());
 
-app.post("/user", async(req, res) => {
+const excludeAUthCheck = require("../config/config");
+
+app.use((req, res, next) => {
+    if (excludeAUthCheck.indexOf(req.method + req.url) > -1) {
+        return next();
+    }
+    if (!req.headers.authorization) {
+        return authFailed(res, "Authorization header missing");
+    }
+    let jwtDecode = decodeJWT(req.headers.authorization);
+    if (!jwtDecode.verify) {
+        return invalidToken(jwtDecode, res);
+    }
+    req.jwtToken = jwtDecode.data;
+    return next();
+});
+
+let invalidToken = (jwtDecode, res) => {
+    let message = "Invalid token.";
+    let statusCode = 401
+    switch (jwtDecode.data.name) {
+        case "TokenExpiredError":
+            message = "Token has been expire.";
+            statusCode = 403;
+            break;
+        default:
+            break;
+
+    }
+    return authFailed(res, message, statusCode);
+}
+
+let decodeJWT = (token) => {
+    token = token.replace(/^Bearer\s/, '');
+    let verify = 0;
+    let data;
+    try {
+        verify = true;
+        data = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+        verify = false;
+        data = { message: err.message, name: err.name };
+    }
+    return { verify, data };
+}
+
+let authFailed = (res, title, statusCode = 401) => {
+    return throwError({
+        code: statusCode,
+        title,
+        source: { pointer: "header.authorization" }
+    }, res, statusCode);
+}
+let apiKeyFailed = (res, title, statusCode = 401) => {
+    return throwError({
+        code: statusCode,
+        title,
+        source: { pointer: "header.x-api-key" }
+    }, res, statusCode);
+}
+app.post("/user", async (req, res) => {
     let userJsonSchema = require('../schema/user-insert.json');
     let ajv = new Ajv();
     let validate = ajv.compile(userJsonSchema);
@@ -38,7 +99,7 @@ app.post("/user", async(req, res) => {
     return internalError(res);
 });
 
-app.post("/user/login", async(req, res) => {
+app.post("/user/login", async (req, res) => {
     let userJsonSchema = require('../schema/user-login.json');
     let ajv = new Ajv();
     let validate = ajv.compile(userJsonSchema);
@@ -49,18 +110,62 @@ app.post("/user/login", async(req, res) => {
     if (user === null || !bcrypt.compareSync(req.body.password, user.password)) {
         return userNotFound(res);
     }
-    let exp = Math.floor(Date.now() / 1000) + (60 * 60);
-    let token = jwt.sign({
-        id: user._id,
-        iss: process.env.JWT_ISS,
-        exp
-    }, process.env.JWT_SECRET);
-
-    return res.status(201).json({
-        token,
-        expire_at: exp
-    });
+    return await tokenReturn(res, user);
 });
+
+app.post("/user/access-token", async (req, res) => {
+    if (req.headers['x-api-key'] !== process.env.APP_SECRET_KEY) {
+        return apiKeyFailed(res, "Invalid api key");
+    }
+    let getToken = await select({ _id: new ObjectID(req.jwtToken.token) }, "refresh_tokens");
+    if (getToken.status !== 1 || unix_timestamp() > getToken.expire_at) {
+        return authFailed(res, "Invalid token.");
+    }
+    let user = await select({ _id: new ObjectID(req.jwtToken.id) }, "users");
+    if (!user) {
+        return userNotFound(res);
+    }
+    return tokenReturn(res, user);
+});
+
+
+
+let tokenReturn = async (res, user) => {
+    let refreshToken = await refreshTokenGen(user);
+    if (!refreshToken) return internalError(res);
+    let lifetime = parseInt(process.env.JWT_ACCESS_LIFETIME);
+    return res.status(201).json({
+        access: {
+            token: genToken({ id: user._id, sub: "access-token" }, lifetime + 10),
+            expire_at: unix_timestamp() + lifetime
+        },
+        refresh: refreshToken
+    });
+}
+
+let refreshTokenGen = async (user) => {
+    let lifetime = parseInt(process.env.JWT_REFRESH_LIFETIME);
+    let token = await insert({
+        app: process.env.APP_SECRET_KEY,
+        user_id: user._id,
+        status: 1,
+        expire_at: (unix_timestamp() + lifetime)
+    }, "refresh_tokens");
+    if (token.insertedCount !== 1) {
+        return false;
+    }
+    return genToken({ id: user._id, token: token.insertedId, sub: "refresh-token" }, lifetime + 10);
+}
+
+let unix_timestamp = () => {
+    return (Math.floor(Date.now() / 1000));
+}
+
+let genToken = (data, seconds = 60) => {
+    return jwt.sign(extend(data, {
+        iss: process.env.JWT_ISS
+    }), process.env.JWT_SECRET, { expiresIn: seconds });
+}
 
 
 let conflictUser = (res) => {
@@ -70,6 +175,7 @@ let conflictUser = (res) => {
         title: "Email address already exits."
     }, res, 409);
 }
+
 let userNotFound = (res) => {
     return throwError({
         code: 404,
@@ -80,7 +186,6 @@ let userNotFound = (res) => {
 let throwValidationError = (validate, res) => {
     errors = [];
     for (let i in validate.errors) {
-        console.log(validate.errors[i].params.missingProperty);
         let pointer = validate.errors[i].params.missingProperty ? validate.errors[i].params.missingProperty : validate.errors[i].dataPath;
         errors.push({
             code: 400,
@@ -100,36 +205,42 @@ let internalError = (res) => {
 
 let throwError = (error, res, statusCode = 400) => {
     let JSONAPIError = require('jsonapi-serializer').Error;
-    let errors = new JSONAPIError(error);
-    return res.status(statusCode).json(errors);
+    return res.status(statusCode).json(new JSONAPIError(error));
 }
 
-let insert = async(data, collection) => {
+let insert = async (data, collection) => {
     try {
         const client = await MongoClient.connect(url, { useUnifiedTopology: true });
         const db = client.db(dbName);
         let insert = db.collection(collection).insertOne(data);
-        // console.log(insert);
         client.close();
         return insert;
     } catch (error) {
-        // console.log(error);
         return error;
     }
 }
-let hasUser = async(email, collection) => {
+let hasUser = async (email, collection) => {
+    return select({ "email": email }, collection)
+}
+let select = async (filter, collection) => {
     try {
         const client = await MongoClient.connect(url, { useUnifiedTopology: true });
         const db = client.db(dbName);
-        let select = db.collection(collection).findOne({ "email": email });
+        let select = db.collection(collection).findOne(filter);
         client.close();
         return select;
     } catch (error) {
-        // console.log(error);
         return error;
     }
 }
 
+// let update = async (filter, update) => {
+//     try{
+//         const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+//         const db = client.db(dbName);
+//         db.collection(collection).updateOne()
+//     }
+// }
 let userJsonOut = (users) => {
     let JSONAPISerializer = require('jsonapi-serializer').Serializer;
 
